12db48894b71064229ccb17ee8325b038c8860f9
Leerena <leerena7@gmail.com>
2023-07-13 10:30:49 +0300

    hw 8 4

---
+++ src/modules/station.js
@@ -0,0 +1,89 @@
+import {Column} from './column';
+import {RenderStation} from './renderStation';
+
+export class Station {
+    #queue = [];
+    #filling = [];
+    #ready = [];
+    constructor(typeStation, renderApp = null) {
+        this.typeStation = typeStation;
+        this.renderApp = renderApp;
+        this.renderStation = null;
+    }
+
+    get filling() {
+        return this.#filling;
+    }
+
+    get queue() {
+        return this.#queue;
+    }
+
+    init() {
+        this.renderColumns();
+
+        this.addStations();
+
+        setInterval(() => {
+            this.checkQueueToFilling();
+        }, 3000);
+    }
+
+    renderColumns() {
+        for (const option of this.typeStation) {
+            for (let i = 0; i < option.count; i++) {
+                this.#filling
+                    .push(new Column(option.type, option.speed));
+            }
+        }
+    }
+
+    addStations() {
+        if (this.renderApp) {
+            this.renderStation = new RenderStation(this.renderApp, this);
+        }
+    }
+
+    checkQueueToFilling() {
+        if (this.#queue.length) {
+            for (let i = 0; i < this.#queue.length; i++) {
+                for (let j = 0; j < this.#filling.length; j++) {
+                    if (!this.#filling[j].car &&
+                        this.#queue[i].typeFuel === this.#filling[j].type) {
+                        this.#filling[j].car = this.#queue.splice(i, 1)[0];
+                        this.fillingGo(this.#filling[j]);
+                        this.renderStation.renderStation();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    fillingGo(column) {
+        const car = column.car;
+        const needPetrol = car.needPetrol;
+        let nowTank = car.nowTank;
+        const timerId = setInterval(() => {
+            nowTank += column.speed;
+            if (nowTank >= car.maxTank) {
+                clearInterval(timerId);
+                const total = nowTank - needPetrol;
+                car.fillUp();
+                column.car = null;
+                this.leaveClient({car, total});
+            }
+        }, 1000);
+        // console.log(`заправляем ${JSON.stringify(column.car)}`);
+    }
+
+    leaveClient({car, total}) {
+        this.#ready.push(car);
+        this.renderStation.renderStation();
+    }
+
+    addCarQueue(car) {
+        this.#queue.push(car);
+        this.renderStation.renderStation();
+    }
+}
